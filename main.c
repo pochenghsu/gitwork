09b595f00f7a34e3fd60be543486612ccdd6835e
pochenghsu <xxxx77799@gmail.com>
2018-05-18 09:16:23 +0800

    123

--- main.c
+++ main.c
@@ -4,12 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int interger1,interger2,interger3;
+	int interGer1,interGer2,interGer3;
 	printf("輸入兩個數字\n");	
-	scanf("%d",&interger1);	 
-	scanf("%d",&interger2);   
-	interger3=interger1+interger2; 
-	printf("和為%d\n",interger3); 
+	scanf("%d",&interGer1);	 
+	scanf("%d",&interGer2);   
+	interGer3=interGer1+interGer2; 
+	printf("和為%d\n",interGer3); 
 	
 	
 	return 0;
