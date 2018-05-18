912bb239cf84ca55cdc74cc869fc6a19b3a3a3f3
pochenghsu <xxxx77799@gmail.com>
2018-05-18 09:21:56 +0800

    123

--- main.c
+++ main.c
@@ -4,12 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int interGer1,interGer2,interGer3;
-	printf("块ㄢ计\n");	
-	scanf("%d",&interGer1);	 
-	scanf("%d",&interGer2);   
-	interGer3=interGer1+interGer2; 
-	printf("㎝%d\n",interGer3); 
+	int interGer1,interGer2,interGer3;	//砞﹚跑计 
+	printf("块ㄢ计\n");	//块计砆计 
+	scanf("%d",&interGer1);	 //块砆计  
+	scanf("%d",&interGer2);  //块计 
+	interGer3=interGer1+interGer2;  //羆 
+	printf("㎝%d\n",interGer3);  //陪ボ羆㎝ 
 	
 	
 	return 0;
